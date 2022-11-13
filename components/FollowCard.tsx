394069ae16097ee9aefeeabd588c75aa64ca6bb1
伊藤新 <itoushin@itoushinnoMacBook-Air.local>
2022-11-13 23:53:53 +0900

    follow

--- components/FollowCard.tsx
+++ components/FollowCard.tsx
@@ -6,7 +6,7 @@ type Props = {
   follow: Follow;
 };
 
-const FollowCard = () => {
+const FollowCard = (props: Props) => {
   return (
     <div className="border-4 border-gray-300 w-1/2 p-4 space-y-3">
       <div className="flex flex-row space-x-6 ">
--- pages/follower.tsx
+++ pages/follower.tsx
@@ -1,17 +1,30 @@
 import React from "react";
 import FollowCard from "../components/FollowCard";
+import { Follow } from "../types/Follower";
 
-const Follower = () => {};
+const Follower = () => {
+  const follower: Follow[] = [
+    {
+      id: 1,
+      followerId: 1,
+      followeeId: 1,
+      createdAt: new Date(),
+    },
+    {
+      id: 2,
+      followerId: 2,
+      followeeId: 2,
+      createdAt: new Date(),
+    },
+  ];
 
-const follower: any = () => {
   return (
-    <>
-      <FollowCard />
-      <FollowCard />
-      <FollowCard />
-      <FollowCard />
-    </>
+    <div>
+      {follower.map((follow, index) => (
+        <FollowCard key={index} follow={follow} />
+      ))}
+    </div>
   );
 };
 
-export default follower;
+export default Follower;
