b7697f70ec855bc9deb20aba08ea9472736046bb
伊藤新 <itoushin@itoushinnoMacBook-Air.local>
2022-11-26 21:47:21 +0900

    error

--- pages/api/task/create.ts
+++ pages/api/task/create.ts
@@ -1,10 +1,10 @@
-import { prisma } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma";
 
 export default async function handleCreate(
   req: NextApiRequest,
   res: NextApiResponse
-) {
+): Promise<void> {
   const result = await prisma.tweet.create({
     data: {
       name: req.body.name,
